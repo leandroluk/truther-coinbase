361518ecb6e21418fbfccac68050aa22de61219b
Leandro Santiago Gomes <leandroluk@gmail.com>
2025-07-06 14:15:53 -0300

    refactor: ignoring barrel files (index.ts) from tests and adding e2e-test

--- packages/config-jest/base.ts
+++ packages/config-jest/base.ts
@@ -3,11 +3,11 @@ import type {Config} from 'jest';
 export default {
   roots: ['src', 'test'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!**/index.ts'],
   coverageDirectory: './coverage',
   coverageProvider: 'v8',
   moduleFileExtensions: ['js', 'ts', 'json'],
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/test/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+  testMatch: ['<rootDir>/test/**/*.{e2e-test,test,spec}.{js,jsx,ts,tsx}'],
   moduleNameMapper: {'^#/(.*)$': '<rootDir>/src/$1'},
 } as const as Config
--- packages/config-jest/nest.ts
+++ packages/config-jest/nest.ts
@@ -4,7 +4,6 @@ import baseConfig from './base';
 export default {
   ...baseConfig,
   transform: {'^.+\\.(t|j)s$': 'ts-jest'},
-  collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
 } as const satisfies Config;
