1e08dd2e7665fa6eb9f1328417b47af3de8ae2d8
Leandro Santiago Gomes <leandroluk@gmail.com>
2025-07-10 16:39:14 -0300

    chore: ignoring main.ts file

--- packages/config-jest/nest.ts
+++ packages/config-jest/nest.ts
@@ -1,8 +1,10 @@
 import type {Config} from 'jest';
 import baseConfig from './base';
 
+
 export default {
   ...baseConfig,
+  collectCoverageFrom: baseConfig.collectCoverageFrom!.concat('!**/main.ts'),
   transform: {'^.+\\.(t|j)s$': 'ts-jest'},
   testEnvironment: 'node',
 } as const satisfies Config;
